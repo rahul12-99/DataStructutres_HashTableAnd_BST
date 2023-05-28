b9a9d9ef3cf5545b220b571bab87367878e22690
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-28 23:37:34 +0530

    Added : Code for find frequency of a word in a paragraph

--- src/com/hashmap/HashMap.java
+++ src/com/hashmap/HashMap.java
@@ -8,19 +8,6 @@ public class HashMap {
     }
 
     /**
-     * Method to count frequency of word in given string
-     */
-    public void countOfOccurrence() {
-        String sentence = "To be or not to be";
-        String[] words = sentence.toLowerCase().split(" ");
-        for (String word : words) {
-            put(word, 1);
-        }
-        int frequency = get("be");
-        System.out.println("Frequency of given key is " + frequency);
-    }
-
-    /**
      * Method to map key,value pair on the hashmap
      */
     public void put(String key, Integer value) {
